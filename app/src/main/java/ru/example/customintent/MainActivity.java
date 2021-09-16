add2608c87c90d58ea6587124cbd124e2c3afe14
Flevertyapp <Flevertyap@gmail.com>
2021-09-16 22:38:28 +0300

    custom intent filter

--- app/src/main/java/ru/example/customintent/MainActivity.java
+++ app/src/main/java/ru/example/customintent/MainActivity.java
@@ -2,13 +2,27 @@ package ru.example.customintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TEXT = "Param";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle == null) {
+            return;
+        }
+        String text = bundle.getString(TEXT); //get data from Intent
+        TextView textView = findViewById(R.id.textEcho);
+        textView.setText(text);
+
     }
 }
